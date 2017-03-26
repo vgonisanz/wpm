e451c0717b4c3cbbe07049049b6888d7aba03c17
Victor Goni <vgonisanz@gmail.com>
2017-03-26 22:46:37 +0200

    Creating general widget, strange class override another BUG

--- examples/widget_sample.py
+++ examples/widget_sample.py
@@ -7,23 +7,63 @@ from curses import wrapper  # Use my own wrapper
 
 from wpm import Wpm
 from widget import Widget
+from widget import ActionObject
 
 # Configuration
+widget_width = 20
+widget_height = 10
+widget_x0 = 1
+widget_y0 = 2
 
 # Variables
+widget = None
+widget_background = None
 
-def main(stdscr):
+def callback_action1(ignore):
+    widget_background.print_message("Left")
+    return None
+
+def callback_c(ignore):
+    widget_background.print_message("c")
+    return None
+
+def callback_quit(ignore):
+    widget.end_condition()
+    return None
+
+def initialize():
+    global wpm
+    global background
+
     wpm = Wpm()
-    background = wpm.get_background()
+    background = wpm.get_background()   # Get main window to print
+    return None
 
-    # Create widget or widget here
-    widget_width = 20
-    widget_height = 10
-    widget_x0 = 1
-    widget_y0 = 2
+def create_widget():
+    global widget
+    global widget_background
 
     widget = Widget(widget_width, widget_height, widget_x0, widget_y0)
-    widget.run_test()
+    widget_background = widget.get_background()
+    action_c = ActionObject(ord('c'), callback_c)
+    #action_quit = ActionObject(ord('q'), callback_quit)
+    background.print_message(str(action_c.character))
+    #background.print_message(str(action_quit.character))
+    background.waitforkey(True, 1, 2)
+    #action1 = ActionObject(curses.KEY_LEFT, callback_action1)
+
+    #widget.add_event(action1)
+    widget.add_event(action_quit)
+    widget.add_event(action_c)
+
+    widget.run()
+    return None
+
+
+
+def main(stdscr):
+    initialize()
+    create_widget()
 
     wpm.msleep(1)
     background.waitforkey(True, 1, 2)
--- src/widgets/widget.py
+++ src/widgets/widget.py
@@ -7,6 +7,16 @@ from curses import wrapper  # Use my own wrapper
 from wpm import Wpm
 from element import Element
 
+class ActionObject(object):
+    character = None
+    action = None
+
+    @classmethod
+    def __init__(self, character_input, action_input):
+        # check type() is type
+        self.character = character_input
+        self.action = action_input
+        return None
 """
 Functionality
 """
@@ -14,11 +24,71 @@ class Widget(object):
 
     _end_condition = False
     _background = None
+    _events = []    # List with trigger and action
 
     @classmethod
     def __init__(self, width, height, x0, y0):
         self._background = Element(width, height, x0, y0)
         return None
+
+    """
+    This private method iterate events while not end condition.
+
+    :return: returns nothing
+    """
+    @classmethod
+    def _iterate_events(self):
+        self._background.change_color(curses.COLOR_BLACK, curses.COLOR_WHITE) # Test
+        self._background.set_input_mode(True)
+        self._end_condition = False
+        while not self._end_condition:
+            event = self._background.get_character()
+            #self._background.print_message(str(len(self._events)))
+            for member in self._events:
+                self._background.print_message(str(member.character))
+                self._background.print_message(" END ")
+                #if event == member.character:
+                #    self._background.print_message(str(event))
+                #    self._background.print_message(str(member.character))
+                #    self._background.print_message("si")
+                #    self._background.print_message("SI")
+                #else:
+                #    self._background.print_message("NO")
+
+                #if event == member.character:
+                    #self._background.print_message(str(member.character))
+                    #member.action()
+        self._background.set_input_mode(False)
+        return None
+
+    """
+    Add a new event. You must send a action object.
+    :input action_object
+    :return: returns nothing
+    """
+    @classmethod
+    def add_event(self, action_object):
+        self._events.append(action_object)
+        return None
+
+    """
+    Set end condition true.
+
+    :return: returns nothing
+    """
+    @classmethod
+    def end_condition(self):
+        self._end_condition = True
+        return None
+
+    """
+    get background
+
+    :return: returns nothing
+    """
+    @classmethod
+    def get_background(self):
+        return self._background
 
     """
     This widget take the control of the UI.
@@ -28,6 +98,7 @@ class Widget(object):
     @classmethod
     def run(self):
         # Override me
+        self._iterate_events()
         return None
 
     """
